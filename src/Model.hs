cca16070b32379e7263b997c516bc78f5bf07844
Ryan Rempel <rgrempel@gmail.com>
2017-06-28 20:55:44 -0500

    Don't use separate file for models DSL.

--- src/Model.hs
+++ src/Model.hs
@@ -5,6 +5,7 @@
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE NoImplicitPrelude          #-}
 {-# LANGUAGE OverloadedStrings          #-}
+{-# LANGUAGE QuasiQuotes                #-}
 {-# LANGUAGE TemplateHaskell            #-}
 {-# LANGUAGE TypeFamilies               #-}
 module Model where
@@ -12,12 +13,39 @@ module Model where
 import ClassyPrelude.Yesod
 import Data.PersistSemVer ()
 import Data.SemVer (Version)
-import Database.Persist.Quasi
 
 
 -- You can define all of your database entities in the entities file.
 -- You can find more information on persistent and how to declare entities
 -- at:
 -- http://www.yesodweb.com/book/persistent/
-share [mkPersist sqlSettings]
-    $(persistFileWith lowerCaseSettings "config/models")
+share
+    [mkPersist sqlSettings]
+    [persistLowerCase|
+        User sql=users
+            name Text
+            email Text
+            avatarUrl Text Maybe
+            plugin Text
+            ident Text
+
+            UniqueUser plugin ident
+            deriving Eq Show Typeable
+
+        Repo sql=repos
+            gitUrl Text
+            submittedBy UserId Maybe
+
+            UniqueRepo gitUrl
+            deriving Eq Show
+
+        RepoVersion
+            repo RepoId
+            tag Text
+            version Version
+            package Text
+            -- TDOD: Use a JSON type?
+
+            UniqueRepoVersion repo version
+            deriving Eq Show
+    |]
