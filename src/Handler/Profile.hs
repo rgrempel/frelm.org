e0e5e671f03cf1c36ac5327b4bd504450b399f94
Ryan Rempel <rgrempel@gmail.com>
2017-07-14 10:39:59 -0600

    Move "Profile" HTML into handler.

--- src/Handler/Profile.hs
+++ src/Handler/Profile.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeFamilies #-}
+
 module Handler.Profile where
 
 import Import.App
@@ -12,4 +13,13 @@ getProfileR = do
     (_, user) <- requireAuthPair
     defaultLayout $ do
         setTitle . toHtml $ userIdent user <> "'s User page"
-        $(widgetFile "profile")
+        [whamlet|
+            <div .ui.container>
+                <h1>Access granted!
+                <p>
+                    This page is protected and access is allowed only for
+                    authenticated users.
+                <p>
+                    Your data is protected with us <strong><span
+                    class="username">#{userIdent user}</span></strong>!
+        |]
