0125cb62c98425ababc8c2e2432cd3812b0671f7
Ryan Rempel <rgrempel@gmail.com>
2017-07-16 17:08:06 -0600

    Group recents page by day.

--- src/Handler/Recent.hs
+++ src/Handler/Recent.hs
@@ -8,12 +8,18 @@
 module Handler.Recent where
 
 import Data.SemVer (toText)
+import Data.Time.Calendar (showGregorian)
 import Database.Esqueleto
 import Import.App hiding (on)
 
 getRecentR :: Handler Html
 getRecentR = do
     result <-
+        fmap
+            (Import.App.groupBy
+                 (\(Entity _ rv, _, _) (Entity _ rv2, _, _) ->
+                      (utctDay . repoVersionCommittedAt) rv ==
+                      (utctDay . repoVersionCommittedAt) rv2)) $
         runDB $
         select $
         distinct $
@@ -25,20 +31,37 @@ getRecentR = do
             orderBy [desc $ repoVersion ^. RepoVersionCommittedAt]
             limit 1000
             pure (repoVersion, package, library)
-    defaultLayout
+    wrapper <- newIdent
+    defaultLayout $ do
         [whamlet|
-            <div .container>
+            <div .container.#{wrapper}>
                 <div .row>
                     <p>
                         This is a list of recently published versions.
                 <div .row>
                     <div .col-lg-12>
-                        <dl>
-                            $forall (Entity rvId rv, Entity _ p, Entity _ l) <- result
-                                <dt>
-                                    #{libraryName l} -
-                                    <a href="@{RepoVersionR rvId}">#{(toText . repoVersionVersion) rv}
-                                    - #{(tshow . repoVersionCommittedAt) rv}
-                                <dd>
-                                    #{packageSummary p}
+                            $forall byDay <- result
+                                $forall (Entity _ rv, Entity _ p, Entity _ l) <- safeHead byDay
+                                    <h4>#{(showGregorian (utctDay (repoVersionCommittedAt rv)))}
+                                    <dl>
+                                        $forall (Entity rvId rv2, Entity _ p, Entity _ l) <- byDay 
+                                            <dt>
+                                                #{libraryName l} -
+                                                <a href="@{RepoVersionR rvId}">#{(toText . repoVersionVersion) rv2}
+                                            <dd>
+                                                #{packageSummary p}
         |]
+        toWidget
+            [cassius|
+                .#{wrapper}
+                    dt
+                        margin-top: 0.5em
+                    dd
+                        margin-left: 3em
+            |]
+
+safeHead :: [a] -> Maybe a
+safeHead a =
+    case a of
+        x:xs -> Just x
+        [] -> Nothing
