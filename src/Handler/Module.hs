9aa2ab7185b6f9f670ecd455a001d9af77dce410
Ryan Rempel <rgrempel@gmail.com>
2017-07-16 22:09:22 -0600

    Make the modules page more interesting.

--- src/Handler/Module.hs
+++ src/Handler/Module.hs
@@ -9,25 +9,63 @@ module Handler.Module where
 
 import Data.SemVer (toText)
 import Database.Esqueleto
-import Import.App
+import Import.App hiding (groupBy, on)
+import qualified Import.App as Prelude
 
 getModulesR :: Handler Html
 getModulesR = do
-    modules <-
+    result <-
+        fmap
+            (Prelude.groupBy
+                 (Prelude.on
+                      (==)
+                      (\(Entity moduleId _, _, _, _, _, _) -> moduleId))) $
         runDB $
         select $
-        from $ \m -> do
-            orderBy [asc $ m ^. ModuleName]
-            pure m
-    defaultLayout
+        from $ \(m `InnerJoin` pm `InnerJoin` p `InnerJoin` l `InnerJoin` rv `InnerJoin` r) -> do
+            on $
+                (r ^. RepoId ==. rv ^. RepoVersionRepo) &&.
+                (just (rv ^. RepoVersionVersion) ==.
+                 sub_select
+                     (from $ \rv2 -> do
+                          where_ $ rv2 ^. RepoVersionRepo ==. r ^. RepoId
+                          pure $ max_ $ rv2 ^. RepoVersionVersion))
+            on $ rv ^. RepoVersionDecoded ==. just (p ^. PackageId)
+            on $ p ^. PackageLibrary ==. just (l ^. LibraryId)
+            on $ pm ^. PackageModulePackageId ==. p ^. PackageId
+            on $ pm ^. PackageModuleModuleId ==. m ^. ModuleId
+            orderBy [asc $ m ^. ModuleName, desc $ rv ^. RepoVersionCommittedAt]
+            pure (m, pm, l, p, rv, r)
+    wrapper <- newIdent
+    defaultLayout $ do
         [whamlet|
-            <div .container>
+            <div .container.#{wrapper}>
                 <div .row>
                     <p>
                         This is a list of all the Elm modules we know about.
                 <div .row>
                     <div .col-lg-12>
-                        $forall Entity moduleId moduleRec <- modules
-                            <div>
-                                #{moduleName moduleRec}
+                        <dl>
+                            $forall byModule <- result
+                                $forall (Entity _ moduleRec, _, _, _, _ ,_) <- safeHead byModule
+                                    <dt>#{moduleName moduleRec}
+                                    <dd>
+                                        $forall (_, _, Entity _ library, _, Entity rvId rv, _) <- byModule
+                                            <div>
+                                                #{libraryName library} /
+                                                <a href="@{RepoVersionR rvId}">#{(toText . repoVersionVersion) rv}
         |]
+        toWidget
+            [cassius|
+                .#{wrapper}
+                    dt
+                        margin-top: 0.5em
+                    dd
+                        margin-left: 3em
+            |]
+
+safeHead :: [a] -> Maybe a
+safeHead a =
+    case a of
+        x:xs -> Just x
+        [] -> Nothing
